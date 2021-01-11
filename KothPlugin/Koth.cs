5cbc524482d0e459fe19873a66fbe51ec3ee461b
IamTails <tom@boob.bot>
2021-01-11 05:20:48 -0500

    fix

--- KothPlugin/Koth.cs
+++ KothPlugin/Koth.cs
@@ -3,11 +3,12 @@ using System;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using Torch;
 using Torch.API;
 using Nest;
-using ParallelTasks;
+
 using Sandbox.ModAPI;
 using Torch.Session;
 using VRage.Game.Entity;
