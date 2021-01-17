702a24ac7574a9ae43214ca50e8ccb9df9740924
Space-Chemist <cleverenz21@gmail.com>
2021-01-17 13:16:06 -0800

    UI updates

--- KothPlugin/KothConfig.cs
+++ KothPlugin/KothConfig.cs
@@ -15,8 +15,21 @@ namespace KothPlugin
 
         private bool _enabled = true;
         private string _webserverUrl = "";
+        private string _host = "";
+        private bool _apienabled = true;
+        
+        public string Host
+        {
+            get => _host;
+            set => SetValue(ref _host, value);
+        }
 
         public bool Enabled { get => _enabled; set => SetValue(ref _enabled, value); }
+        public bool ApiEnabled
+        {
+            get => _apienabled;
+            set => SetValue(ref _apienabled, value);
+        }
 
         public string WebserverUrl { get => _webserverUrl; set => SetValue(ref _webserverUrl, value); }
 
