06cbb076095a738a7f50f77f89e911491c27d8ef
Space-Chemist <cleverenz21@gmail.com>
2021-01-21 23:02:17 -0800

    working commit

--- KothPlugin/KothCommands.cs
+++ KothPlugin/KothCommands.cs
@@ -5,10 +5,10 @@ using VRage.Game.ModAPI;
 
 namespace KothPlugin
 {
-    [Category("Koth")]
+    [Category("koth")]
     public class KothCommands : CommandModule
     {
-        private static readonly Logger Log = LogManager.GetLogger("Koth");
+        private static readonly Logger Log = LogManager.GetLogger("koth");
 
         public KothPlugin.Koth Plugin
         {
@@ -18,13 +18,28 @@ namespace KothPlugin
             }
         }
 
-        [Command("clearscores", "This clears koth scores", null)]
+        [Command("clearscores", "This clears koth scores", "clears koth scores")]
         [Permission(MyPromoteLevel.Admin)]
         public void ClearScores()
         {
             this.Context.Respond("Scores have been Cleared");
             NetworkService.SendPacket("clear");
         }
-
+        
+        [Command("refreshpath", "This clears koth scores", "Used when changing Saved files but not relaunching torch")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void PathRefresher()
+        {
+            this.Context.Respond("Save Path Refreshed");
+            Koth.SetPath();
+        }
+        
+        [Command("testwebhook", "This clears koth scores", "Sends a test message to Webhook")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void WebHookTest()
+        {
+            this.Context.Respond("test message sent");
+            DiscordService.SendDiscordWebHook("Successful WebHook Test");
+        }
     }
 }
